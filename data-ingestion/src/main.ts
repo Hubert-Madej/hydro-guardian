36a9a33110c3486515878bfe549b88903a3e8a75
hmadej <hubertmadej02@gmail.com>
2023-11-18 16:03:44 +0100

    Moved data ingestion service configuration to env files

--- data-ingestion/src/main.ts
+++ data-ingestion/src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { MainDataIngestionModule } from './main-data-ingestion.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainDataIngestionModule);
 
+  const configService = app.get(ConfigService);
+
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.MQTT,
     options: {
-      url: 'mqtt://localhost:1883',
+      url: `mqtt://${configService.get('MQTT_HOST')}:${configService.get(
+        'MQTT_PORT',
+      )}`,
     },
   });
 
